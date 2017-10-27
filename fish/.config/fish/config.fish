761ff26e7e746b663ab3084c79adbae03a731b33
Alexandre Perez <alexandrecperez@gmail.com>
2017-10-27 13:33:40 +0100

    Add zathura abbreviation in fish.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,6 +7,7 @@ abbr --add gd 'git diff'
 abbr --add gst 'git status'
 abbr --add p 'pass'
 abbr --add pc 'pass -c'
+abbr --add z 'zathura --fork'
 
 setenv SSH_AUTH_SOCK $XDG_RUNTIME_DIR/gnupg/S.gpg-agent.ssh
 gpg-connect-agent updatestartuptty /bye > /dev/null
