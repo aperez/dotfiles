222f8b923484d4f0b84e7ff77578d950849fd698
Alexandre Perez <alexandrecperez@gmail.com>
2017-10-27 13:55:41 +0100

    Update pass abbreviation in fish.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,8 +5,7 @@ abbr --add gc 'git commit'
 abbr --add gca 'git commit -a'
 abbr --add gd 'git diff'
 abbr --add gst 'git status'
-abbr --add p 'pass'
-abbr --add pc 'pass -c'
+abbr --add p 'pass -c'
 abbr --add z 'zathura --fork'
 
 setenv SSH_AUTH_SOCK $XDG_RUNTIME_DIR/gnupg/S.gpg-agent.ssh
