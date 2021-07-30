a1d2fdb0adf31fbf38cc3324a1a24fd53759c2d1
Alexandre Perez <alexandrecperez@gmail.com>
2021-07-30 09:48:28 -0700

    Add autojump and fish abbreviations.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,8 +8,13 @@ abbr --add gst 'git status'
 abbr --add p 'pass -c'
 abbr --add z 'zathura --fork'
 
+abbr --add enc 'emacsclient -nc'
+abbr --add en 'emacsclient -n'
+
 setenv SSH_AUTH_SOCK $XDG_RUNTIME_DIR/gnupg/S.gpg-agent.ssh
 gpg-connect-agent updatestartuptty /bye > /dev/null
 
 setenv ALTERNATE_EDITOR ""
+
+[ -f /usr/share/autojump/autojump.fish ]; and source /usr/share/autojump/autojump.fish
 
