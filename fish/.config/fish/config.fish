4b20ecb34c0bfb714b9af976bb83a04aeb9d3131
Alexandre Perez <alexandrecperez@gmail.com>
2025-09-05 16:06:23 -0700

    fish: add aliases for cat and ls. configure fzf commands.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,13 @@
 set fish_greeting ""
 
+alias cat="bat"
+
+alias ls="eza --group-directories-first"
+alias ll="eza --group-directories-first --long --git --all --header"
+alias la="eza --group-directories-first --long --git --all --header"
+
+alias tree="eza --tree"
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
     if test -z (pgrep ssh-agent)
@@ -8,6 +16,13 @@ if status is-interactive
         set -Ux SSH_AGENT_PID $SSH_AGENT_PID
     end
 end
+
+set -gx FZF_DEFAULT_COMMAND "fd --hidden --follow --exclude='**/.git/'"
+
+set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND "--type=f"
+set -gx FZF_CTRL_T_OPTS "--preview='bat --style=numbers --color=always {}'"
+set -gx FZF_ALT_C_COMMAND $FZF_DEFAULT_COMMAND "--type=d"
+set -gx FZF_ALT_C_OPTS "--preview='eza --tree --all --color=always --git-ignore {}'"
 
 fzf --fish | source
 zoxide init fish | source
